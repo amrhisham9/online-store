ae89493e0e79005855eddccc528c83cfb238d704
amr <amrhisham99@gmail.com>
2020-05-05 19:46:38 +0200

    first commit

--- src/index.js
+++ src/index.js
@@ -1,13 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {BrowserRouter as Router} from 'react-router-dom'
 import './index.css';
 import App from './App';
+import {ProductProvider} from './context'
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
-  <React.StrictMode>
+  <ProductProvider>
+  <Router>
     <App />
-  </React.StrictMode>,
+  </Router>
+  </ProductProvider>,
   document.getElementById('root')
 );
 
